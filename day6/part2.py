10ba0eb953a6694fe778e6bc9fbdd8f614588ed4
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2025-12-27 16:29:44 +0530

    Format d6p2

--- day6/part2.py
+++ day6/part2.py
@@ -2,8 +2,10 @@ from pathlib import Path
 import time
 from functools import reduce
 
+
 def product(iterable):
-    return reduce(lambda x, y: x*y, iterable)
+    return reduce(lambda x, y: x * y, iterable)
+
 
 start_time = time.perf_counter()
 
@@ -21,34 +23,33 @@ grand_total = 0
 op_line = inp[num_count]
 
 for i, op in enumerate(op_line):
-    if op == ' ':
+    if op == " ":
         continue
     for j, next_op in enumerate(op_line[i:]):
         if len(op_line[i:]) - 1 == j:
             j += 1
         if j == 0:
             continue
-        if next_op == ' ':
+        if next_op == " ":
             continue
         j -= 1
         break
-    start, end = i, i+j
+    start, end = i, i + j
     block = []
     for j in range(num_count):
         x = inp[j][start:end]
         block.append(x)
 
-    block_result = 0 if op == '+' else 1
-    
+    block_result = 0 if op == "+" else 1
+
     for j in range(len(block[0])):
-        num = ''.join([k[j] for k in block])
+        num = "".join([k[j] for k in block])
         num = int(num)
-        if op == '+':
+        if op == "+":
             block_result += num
-        if op == '*':
+        if op == "*":
             block_result *= num
     grand_total += block_result
-
 
 
 end_time = time.perf_counter()
